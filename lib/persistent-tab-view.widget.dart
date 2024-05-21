1f9b6e3504b9dce3b2be30d5563c4ed4a89281b7
Alina Dodi <alina.dodi@ebs-integrator.com>
2024-05-21 13:25:38 +0300

    remove unused code

--- lib/persistent-tab-view.widget.dart
+++ lib/persistent-tab-view.widget.dart
@@ -701,51 +701,7 @@ class _PersistentTabViewState extends State<PersistentTabView> {
         },
         child: navigationBarWidget(),
       );
-    }
-
-    /* if (widget.handleAndroidBackButtonPress || widget.onWillPop != null) {
-      return WillPopScope(
-        onWillPop: !widget.handleAndroidBackButtonPress &&
-                widget.onWillPop != null
-            ? widget.onWillPop!(_contextList[_controller!.index])
-                as Future<bool> Function()?
-            : widget.handleAndroidBackButtonPress && widget.onWillPop != null
-                ? () async {
-                    if (_controller!.index == 0 &&
-                        !Navigator.canPop(_contextList.first!)) {
-                      return widget.onWillPop!(_contextList.first);
-                    } else {
-                      if (Navigator.canPop(_contextList[_controller!.index]!)) {
-                        Navigator.pop(_contextList[_controller!.index]!);
-                      } else {
-                        if (widget.onItemSelected != null) {
-                          widget.onItemSelected!(0);
-                        }
-                        _controller!.index = 0;
-                      }
-                      return false;
-                    }
-                  }
-                : () async {
-                    if (_controller!.index == 0 &&
-                        !Navigator.canPop(_contextList.first!)) {
-                      return true;
-                    } else {
-                      if (Navigator.canPop(_contextList[_controller!.index]!)) {
-                        Navigator.pop(_contextList[_controller!.index]!);
-                      } else {
-                        if (widget.onItemSelected != null) {
-                          widget.onItemSelected!(0);
-                        }
-                        _controller!.index = 0;
-                      }
-                      return false;
-                    }
-                  },
-        child: navigationBarWidget(),
-      );
-    }*/
-    else {
+    } else {
       return navigationBarWidget();
     }
   }
